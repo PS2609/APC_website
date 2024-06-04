ccf347d0e90aaa8dd49a63b56a61d424e3a6849e
coderShivam45 <shivamsingh45612360@gmail.com>
2024-06-04 10:57:25 +0530

    responsiveness added

--- src/components/banner1.jsx
+++ src/components/banner1.jsx
@@ -6,20 +6,17 @@ function Banner1(){
     return (
        <>
        <div className='w-full  flex items-center px-0 py-0 '  >
-          <img src={astronaut} className="md:w-full w-full md:py-0" />
+          
+          <img src={astronaut} className=" object-cover md:w-full w-full md:py-0 h-screen" />
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-304 text-white font-bold text-center">
-          <h3 className='text-7xl md:3xl lg:7xl'>Welcome to APC !</h3>
+          <h3 className='text-5xl md:3xl lg:7xl'>Welcome to APC !</h3>
         </div>
         <div className="absolute bottom-0 left-0 w-full  bg-black bg-opacity-60 text-white flex items-center justify-center">
           <p className="text-md text-2 rem font-medium">
             "The universe is a pretty big place. If it's just us, seems like an awful waste of space." <br className="text-md "/>- Carl Sagan
           </p>
         </div> 
-        <div className='container'>
-        <div className="container mx-auto text-center py-10">
-                    <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">About Us</h1>
-                console.log("hey")</div>
-        </div>
+        
           </div>
        </>
     )
