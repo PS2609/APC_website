7a0c1c32532aa8b10846be8a2c601b40c7b73e05
coderShivam45 <shivamsingh45612360@gmail.com>
2024-06-06 11:08:16 +0530

    Teams Page added

--- src/components/teams.jsx
+++ src/components/teams.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import Card from './teamsCard'
 import blankimage from '../images/blank_image.png'
+import PrakharGupta from '../images/TeamMembers/Prakhar Gupta1.jpg'
+import HarshLoomba from '../images/TeamMembers/Harsh Loomba.png'
+import AmoyAshesh from '../images/TeamMembers/Amoy Ashesh.jpg'
+import Awantika from '../images/TeamMembers/Awantika.png'
+import Khushal_Yadav from "../images/TeamMembers/Khushal Yadav(OC).png"
+import Harsh_Dahiya from "../images/TeamMembers/Harsh_Dahiya(Education and Outreach).png"
+import  Subham_Prasad_Gouda from "../images/TeamMembers/Subham Prasad Gouda (PR & Magazine).jpg"
+import  Suprajit  from "../images/TeamMembers/Suprajit (PR&Magazine).jpg"
+import  Utkarsh_Arya  from "../images/TeamMembers/Utkarsh Arya.jpg"
+import  Priyanshi_Sharma from "../images/TeamMembers/Priyanshi_Sharma.jpg"
+
+
 const teams = () => {
-    const position = ["", ""]
     return (
 
         <div className="bg-gradient-to-b from-[#050023] via-[#0E1700] to-[#1B0B7D]  p-5 text-white">
@@ -12,17 +23,16 @@ const teams = () => {
             <div className="advisory">
                 <h1 className="text-3xl md:text-4xl text-center py-10 ">ADVISORY (2024-2025)</h1>
                 <div className="profiles flex justify-center gap-8 flex-wrap ">
-                    <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                    <Card image={blankimage} name={"NAME"} position={"POSITION"} />
+                    <Card image={PrakharGupta} name={"Prakhar Gupta"} />
+                    <Card image={HarshLoomba} name={"Harsh Loomba"} />
+                    <Card image={AmoyAshesh} name={"Amoy Ashesh"} />
+                    <Card image={Awantika} name={"Awantika"} />
                 </div>
             </div>
             <div className="O.C (2024-2025)">
                 <h1 className="text-3xl md:text-4xl text-center py-10 ">O.C (2024-2025)</h1>
                 <div className="profiles flex justify-center gap-8 flex-wrap ">
-                    <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                    <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                    <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                    <Card image={blankimage} name={"NAME"} position={"POSITION"} />
+                    <Card image={Khushal_Yadav} name={"Khushal Yadav"} />
                 </div>
             </div>
             <div className='EDUCATION & OUTREACH'>
@@ -30,17 +40,17 @@ const teams = () => {
                 <div className="cordinator">
                     <h1 className="text-3xl md:text-4xl text-center py-10 ">COORDINATOR</h1>
                     <div className="profiles flex justify-center gap-8 flex-wrap ">
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
+                        <Card image={Harsh_Dahiya} name={"Harsh Dahiya"} />
+                        
                     </div>
                 </div>
                 <div className="sub-cordinator">
                     <h1 className="text-3xl md:text-4xl text-center py-10 ">SUB-COORDINATOR</h1>
                     <div className="profiles flex justify-center gap-8 flex-wrap ">
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
+                        <Card image={blankimage} name={"NAME"} />
+                        <Card image={blankimage} name={"NAME"} />
+                        <Card image={blankimage} name={"NAME"} />
+                        <Card image={blankimage} name={"NAME"} />
                     </div>
                 </div>
             </div>
@@ -49,17 +59,17 @@ const teams = () => {
                 <div className="cordinator">
                     <h1 className="text-3xl md:text-4xl text-center py-10 ">COORDINATOR</h1>
                     <div className="profiles flex justify-center gap-8 flex-wrap ">
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
+                        <Card image={Priyanshi_Sharma} name={"Priyanshi Sharma"} />
+                        <Card image={Utkarsh_Arya} name={"Utkarsh Arya"} />
                     </div>
                 </div>
                 <div className="sub-cordinator">
                     <h1 className="text-3xl md:text-4xl text-center py-10 ">SUB-COORDINATOR</h1>
                     <div className="profiles flex justify-center gap-8 flex-wrap ">
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
+                        <Card image={blankimage} name={"NAME"} />
+                        <Card image={blankimage} name={"NAME"} />
+                        <Card image={blankimage} name={"NAME"} />
+                        <Card image={blankimage} name={"NAME"} />
                     </div>
                 </div>
             </div>
@@ -68,17 +78,17 @@ const teams = () => {
                 <div className="cordinator">
                     <h1 className="text-3xl md:text-4xl text-center py-10 ">COORDINATOR</h1>
                     <div className="profiles flex justify-center gap-8 flex-wrap ">
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
+                        <Card image={Subham_Prasad_Gouda} name={"Subham Prasad Gouda"} />
+                        <Card image={Suprajit} name={"Suprajit"} />
                     </div>
                 </div>
                 <div className="sub-cordinator">
                     <h1 className="text-3xl md:text-4xl text-center py-10 ">SUB-COORDINATOR</h1>
                     <div className="profiles flex justify-center gap-8 flex-wrap ">
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
-                        <Card image={blankimage} name={"NAME"} position={"POSITION"} />
+                        <Card image={blankimage} name={"NAME"} />
+                        <Card image={blankimage} name={"NAME"} />
+                        <Card image={blankimage} name={"NAME"} />
+                        <Card image={blankimage} name={"NAME"} />
                     </div>
                 </div>
             </div>
--- src/components/teamsCard.jsx
+++ src/components/teamsCard.jsx
@@ -3,12 +3,12 @@ const teamsCard = (props) => {
     return (
         <>
             <div className="card w-[300px] h-[350px] bg-[#00181B] rounded-3xl p-5  ">
-                <div className="img mx-auto">
+                <div className="img mx-auto object-cover">
                     <img src={props.image} alt="" className='w-[170px] h-[190px]' />
                 </div>
                 <div className="text my-auto text-center text-xl  md:text-3xl">
                     <div className="name ">{props.name}</div>
-                    <div className="name">{props.position}</div>
+                    {/* <div className="name">{props.position}</div> */}
                 </div>
                 
             </div>
